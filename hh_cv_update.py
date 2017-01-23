f43df959a140c56d542eaef6516dc10127610e61
larscliff <larscliff@gmail.com>
2017-01-23 23:07:42 +0300

    Update hh_cv_update.py

--- hh_cv_update.py
+++ hh_cv_update.py
@@ -2,34 +2,34 @@
 
 from selenium import webdriver
 
-# authentication details
+# Authentication details
 LOGIN = input("Please type your email  ---> ")
 PASSWORD = input("Please type your password ---> ")
 
-# chrome is default browser, change to appropriated one
+# Chrome is a default browser, change to appropriated one
 browser = webdriver.Chrome()
 browser.get('https://www.hh.ru/account/login')
 
-# authentication
+# Authentication
 emailel = browser.find_element_by_css_selector('input[type="email"]')
 emailel.send_keys(LOGIN)
 passel = browser.find_element_by_css_selector('input[type="password"]')
 passel.send_keys(PASSWORD)
 passel.submit()
 
-# parse links of all CVs
+# Looking for CV's links
 browser.get('https://hh.ru/applicant/resumes')
 links = []
 resume_els = browser.find_elements_by_css_selector('a.b-resumelist-vacancyname')
 for r in resume_els:
 	links.append(r.get_attribute('href'))
 
-# update all CVs
+# Update all CVs
 for link in links:
     browser.get(link)
     refresh_button = browser.find_element_by_css_selector('button.HH-\
 Resume-Touch-Button')
     refresh_button.click()
     
-# quit webdriver
+# Quit webdriver
 browser.quit()
