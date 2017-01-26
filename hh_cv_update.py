eff4babcab88c8556bb05da417e46e7eb3939f2b
larscliff <larscliff@gmail.com>
2017-01-26 10:22:54 +0300

    Update hh_cv_update.py

--- hh_cv_update.py
+++ hh_cv_update.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
+# Sctipt to auto update all your CVs on hh.ru
 
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+# Constants
+CSS_SEL = 'button.HH-Resume-Touch-Button'
+DELAY = 10
 
 # Authentication details
-LOGIN = input("Please type your email  ---> ")
-PASSWORD = input("Please type your password ---> ")
+login = input("Please type your email  ---> ")
+password = input("Please type your password ---> ")
 
 # Chrome is a default browser, change to appropriated one
 browser = webdriver.Chrome()
@@ -12,23 +20,28 @@ browser.get('https://www.hh.ru/account/login')
 
 # Authentication
 emailel = browser.find_element_by_css_selector('input[type="email"]')
-emailel.send_keys(LOGIN)
+emailel.send_keys(login)
 passel = browser.find_element_by_css_selector('input[type="password"]')
-passel.send_keys(PASSWORD)
+passel.send_keys(password)
 passel.submit()
 
 # Looking for CV's links
 browser.get('https://hh.ru/applicant/resumes')
 links = []
-resume_els = browser.find_elements_by_css_selector('a.b-resumelist-vacancyname')
+resume_els = browser.find_elements_by_css_selector(\
+    'a.b-resumelist-vacancyname')
 for r in resume_els:
 	links.append(r.get_attribute('href'))
 
 # Update all CVs
 for link in links:
     browser.get(link)
-    refresh_button = browser.find_element_by_css_selector('button.HH-Resume-Touch-Button')
-    refresh_button.click()
-    
+    refresh_button = WebDriverWait(browser, DELAY).until(\
+        EC.presence_of_all_elements_located((By.CSS_SELECTOR, CSS_SEL)))
+    try:
+        refresh_button[0].click()
+    except:
+        refresh_button[1].click()
+
 # Quit webdriver
 browser.quit()
